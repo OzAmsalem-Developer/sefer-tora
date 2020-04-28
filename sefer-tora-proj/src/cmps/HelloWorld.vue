a0a1e0807eba0530dea41ba3de46b1f52b07f4f5
OzAmsalem-Developer <ozamsalem8@gmail.com>
2020-04-28 13:29:47 +0300

    s

---
+++ sefer-tora-proj/src/cmps/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello" v-if="perekA">
+    {{perekA}}
+  </div>
+</template>
+
+<script>
+import {pasukService} from '@/services/psukim.service.js'
+
+export default {
+data() {
+  return {
+    perekA: pasukService.txt
+  }
+},
+
+}
+</script>
+
